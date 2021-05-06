ee513b26640b99ab17f28bfb229e10f4fef1fcc4
Johnny <johnnie@Johnnys-MBP.T-mobile.com>
2021-05-06 10:49:50 -0700

    Base Created

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
+'''
+Johnny Huynh
+Dylan P
+CST205 
+Final Project: Blog/Learning App
+'''
+
+#Imports
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
+from posts_info import posts_info
 
+#Creating flask instance
 app = Flask(__name__)
+
+#Creating bootstrap instance
 boostrap = Bootstrap(app)
 
-
-@app.route('/')
+#Homepage Route
+@app.route('/', methods=['GET','POST'])
 def home():
-    return render_template('template.html')
+    #Return
+    return render_template("template.html", imgList=updateList)
+
+#Details Route
+@app.route('/details/<pid>')
+def details(pid):
+    #Open Image
+    image = Image.open(f"static/images/{pid}.jpg")
+    #All variables that will be sent
+   	week=""
+    subject=""
+    description=""
+    details=""
+    #Loop and find matching
+    for x in posts_info:
+        if (x['id'] == pid):
+            week = x['week']
+            subject = x['subject']
+            description = x['description']
+            details = x['details']
+    #Return
+    return render_template("details.html", imgList=posts_info, pid=pid, im=image, wk=week, sub=subject, desc=description, dets=details)
+
+
+#Run App
+if __name__ == "__main__":
+    app.run()
 
---
+++ posts_info.py
@@ -0,0 +1,22 @@
+'''
+Johnny Huynh, Dylan
+CST205
+posts_info: has all the data for the images
+4/16/21
+'''
+posts_info = [
+	{
+		"id" : "PLACEHOLDER",
+		"week" : "1",
+		"subject" : "Command Line",
+		"description" : "Make use of the terminal emulator and its importance",
+		"details" : "A text based interface to the computer. The command line is a blank line and cursor on the screen, allowing the user to type in instructions for immediate execution. All major operating systems (Windows, Mac, Unix, Linux, etc.) support command lines that programmers and power users can employ to perform file management operations."
+	},
+	{
+		"id" : "PLACEHOLDER2",
+		"week" : "2",
+		"subject" : "Python Language",
+		"description" : "Popular programming language, easy to learn and read",
+		"details" : "Python Details PLACEHOLDER"
+	}
+]
